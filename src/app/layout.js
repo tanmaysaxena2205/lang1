c9f4a91e55ea4d9089e89c3988079bda1c96ae31
tanmaysaxena22 <tanmaysaxena0589@gmail.com>
2026-02-19 12:06:39 +0000

    Updated project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar"; // Make sure you created this file!
+import Navbar from "@/components/Navbar";
 import { ClerkProvider } from '@clerk/nextjs';
 
 const geistSans = Geist({
@@ -18,13 +18,21 @@ export const metadata = {
   description: "Personalized language learning platform",
 };
 
-
-
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className="bg-black text-white">
+        <head>
+          {/* 1. Preconnect to Google Auth - This makes the popup appear instantly */}
+          <link rel="preconnect" href="https://accounts.google.com" />
+          
+          {/* 2. Preconnect to Clerk's Frontend API */}
+          <link rel="preconnect" href="https://clerk.lang-peach.vercel.app" />
+          
+          {/* 3. DNS Prefetch as a fallback */}
+          <link rel="dns-prefetch" href="https://accounts.google.com" />
+        </head>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}>
           <Navbar />
           {children}
         </body>
