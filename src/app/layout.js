6e7679af150eeb653c08dc1360f17b29730ce722
tanmaysaxena2205 <tanmaysaxena22may@gmail.com>
2026-02-28 22:24:56 +0000

    Updated project

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ClerkProvider } from '@clerk/nextjs';
+import CookieCleaner from "@/components/CookieCleaner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
